0e8a251ecec5d55cea2e2d4da57ec77f42cc0a91
Chudasama1765 <rahulchudasama192612@gmail.com>
2026-01-22 17:13:34 +0530

    extra

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99089d6126826aa411c28073015e1c38055187a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b053f4ca4634f0b3f3988fe4f21d869c08e2a203")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
